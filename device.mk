41d63ebece1046d971a03e3692344a1a9ab35449
AXP.OS <sherlock@binbash.rocks>
2025-03-26 18:05:15 +0100

    bonito: remove reserved space for AXP.OS

--- device.mk
+++ device.mk
@@ -293,7 +293,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # OEM Unlock reporting
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.oem_unlock_supported=1
+    ro.oem_unlock_supported=0
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.cne.feature=1 \
@@ -314,7 +314,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.sap_silent_pin=1 \
     persist.vendor.radio.no_wait_for_card=1 \
     persist.vendor.radio.manual_nw_rej_ct=1 \
-    persist.rcs.supported=1 \
+    persist.rcs.supported=0 \
     vendor.rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so \
     ro.hardware.keystore_desede=true \
 
